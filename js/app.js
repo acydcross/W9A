d8488bd97915f868019e83ee96acf78c634551ea
acydcross <acydcross@hotmail.com>
2020-08-20 19:27:40 -0600

    Simple User Array

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
-var is_subscribed = true;
-var userAge="18cd";
+//var is_subscribed = true;
+//var userAge="18cd";
 
-if((userAge < 18) && (is_subscribed != true)) {
-    console.log("the user is younger than 18 and not subscribed");
+
+//if((userAge < 18) && (is_subscribed != true)) {
+//    console.log("the user is younger than 18 and not subscribed");
+//}
+//else if((userAge >= 18) && (is_subscribed != true)) {
+//    console.log("the user is 18 and older and not subscribed");
+//}
+//else if((userAge < 18) && (is_subscribed == true)) {
+//    console.log("the user is younger than 18 and is subscribed");
+//}
+//else if((userAge >= 18) && (is_subscribed == true)) {
+//    console.log("the user is 18 and older and is subscribed");
+//}
+
+
+var userAge = ["16", "18", "21"];
+var arrayLength = userAge;
+var subscriptionStatus = ["not subscribed", "pending subscription", "subscribed"]
+var arrayLength = subscriptionStatus;
+
+for(var index=0; index < userAge.length; index++) {
+    console.log(userAge[index]);
 }
-else if((userAge >= 18) && (is_subscribed != true)) {
-    console.log("the user is 18 and older and not subscribed");
-}
-else if((userAge < 18) && (is_subscribed == true)) {
-    console.log("the user is younger than 18 and is subscribed");
-}
-else if((userAge >= 18) && (is_subscribed == true)) {
-    console.log("the user is 18 and older and is subscribed");
+
+for(var index=0; index < subscriptionStatus.length; index++) {
+    console.log(subscriptionStatus[index]);
 }
